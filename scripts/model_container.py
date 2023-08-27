7510b893951d80f5dfc25f8116f75ebf286ee232
mille055 <46400739+mille055@users.noreply.github.com>
2023-08-27 16:50:27 -0400

    Update model_container.py

--- scripts/model_container.py
+++ scripts/model_container.py
@@ -6,14 +6,14 @@ from cnn.cnn_inference import load_pixel_model
 
 class ModelContainer:
     def __init__(self):
-        self.cnn_model = load_pixel_model(model_paths['cnn'])
-        self.nlp_model = self.load_model(model_paths['nlp'])
-        self.metadata_model = self.load_model(model_paths['meta'])
-        self.metadata_scaler = self.load_model(model_paths['scaler'])
+        self.cnn_model = load_pixel_model(model_paths2['cnn'])
+        self.nlp_model = self.load_model(model_paths2['nlp'])
+        self.metadata_model = self.load_model(model_paths2['meta'])
+        self.metadata_scaler = self.load_model(model_paths2['scaler'])
         
         #fusion model weights
-        self.fusion_weights_path = model_paths['fusion']
-        self.partial_fusion_weights_path = model_paths['fusion_no_nlp']
+        self.fusion_weights_path = model_paths2['fusion']
+        self.partial_fusion_weights_path = model_paths2['fusion_no_nlp']
 
     def load_model(self, model_path):
         with open(model_path, 'rb') as file:
